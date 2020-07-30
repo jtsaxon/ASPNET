d4fef00bb83ada3b65e12d073ee7f5593f04f4ed
jtsaxon <jaketsaxon@gmail.com>
2020-07-30 10:54:19 -0500

    Steps 4 & 5 Complete

--- ASPNET/IProductRepository.cs
+++ ASPNET/IProductRepository.cs
@@ -13,5 +13,13 @@ namespace ASPNET
         public Product GetProduct(int id);
 
         public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
     }
 }
